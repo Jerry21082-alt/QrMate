2e9b8c0a2daafb119b3fd24a761ba5ff6dff5f6d
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-05-13 18:03:14 +0100

    initial commit

--- app/pages/create/page.jsx
+++ app/pages/create/page.jsx
@@ -1,128 +1,128 @@
-"use client";
+// "use client";
 
-import Nav from "@/components/Nav";
-import Wrapper from "@/components/Wrapper";
-import { pdf, share, ticket, website } from "@/public";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { exportContext } from "@/components/useStateContext/StateContext";
+// import Nav from "@/components/Nav";
+// import Wrapper from "@/components/Wrapper";
+// import { pdf, share, ticket, website } from "@/public";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { exportContext } from "@/components/useStateContext/StateContext";
 
-const selectQrcode = [
-  { icon: website, type: "website" },
-  { icon: share, type: "social media" },
-  { icon: ticket, type: "coupon" },
-  { icon: pdf, type: "PDF" },
-];
+// const selectQrcode = [
+//   { icon: website, type: "website" },
+//   { icon: share, type: "social media" },
+//   { icon: ticket, type: "coupon" },
+//   { icon: pdf, type: "PDF" },
+// ];
 
-export default function CreatePage() {
-  const [selectFormat, setSelectFormat] = useState(0);
-  const { regenerateQrcode, headingTxt, type, setType } = exportContext();
+// export default function CreatePage() {
+//   const [selectFormat, setSelectFormat] = useState(0);
+//   const { regenerateQrcode, headingTxt, type, setType } = exportContext();
 
-  const handleSelectFormat = (id, type) => {
-    setSelectFormat(id);
-    setType(type);
-  };
+//   const handleSelectFormat = (id, type) => {
+//     setSelectFormat(id);
+//     setType(type);
+//   };
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const handleCreateCancel = () => {
-    router.push("/pages/home");
-    regenerateQrcode();
-  };
+//   const handleCreateCancel = () => {
+//     router.push("/pages/home");
+//     regenerateQrcode();
+//   };
 
-  return (
-    <Wrapper>
-      <Nav />
-      <div className="flex justify-between items-center mt-14">
-        <button
-          onClick={handleCreateCancel}
-          className="w-[120px] h-[40px] rounded-md bg-white py-1 px-4 flex justify-center items-center space-x-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 30 30"
-            width="20px"
-            height="20px"
-          >
-            {" "}
-            <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z" />
-          </svg>{" "}
-          <p className="text-sm">Cancel</p>
-        </button>
+//   return (
+//     <Wrapper>
+//       <Nav />
+//       <div className="flex justify-between items-center mt-14">
+//         <button
+//           onClick={handleCreateCancel}
+//           className="w-[120px] h-[40px] rounded-md bg-white py-1 px-4 flex justify-center items-center space-x-2"
+//         >
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 30 30"
+//             width="20px"
+//             height="20px"
+//           >
+//             {" "}
+//             <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z" />
+//           </svg>{" "}
+//           <p className="text-sm">Cancel</p>
+//         </button>
 
-        <Link
-          href={{
-            query: { type: type },
-            pathname: `/pages/previewQrcode/${type}`,
-          }}
-          className="rounded-md bg-red py-1 px-4 flex justify-center items-center space-x-2 w-[120px] h-[40px]"
-        >
-          <p className="text-white text-sm">Next</p>
+//         <Link
+//           href={{
+//             query: { type: type },
+//             pathname: `/pages/previewQrcode/${type}`,
+//           }}
+//           className="rounded-md bg-red py-1 px-4 flex justify-center items-center space-x-2 w-[120px] h-[40px]"
+//         >
+//           <p className="text-white text-sm">Next</p>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            id="chevron"
-            width={35}
-            height={35}
-            fill="#FFFFFF"
-          >
-            <path d="M11 10L7.859 6.58a.695.695 0 0 1 0-.978.68.68 0 0 1 .969 0l3.83 3.908a.697.697 0 0 1 0 .979l-3.83 3.908a.68.68 0 0 1-.969 0 .695.695 0 0 1 0-.978L11 10z"></path>
-          </svg>
-        </Link>
-      </div>
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 20 20"
+//             id="chevron"
+//             width={35}
+//             height={35}
+//             fill="#FFFFFF"
+//           >
+//             <path d="M11 10L7.859 6.58a.695.695 0 0 1 0-.978.68.68 0 0 1 .969 0l3.83 3.908a.697.697 0 0 1 0 .979l-3.83 3.908a.68.68 0 0 1-.969 0 .695.695 0 0 1 0-.978L11 10z"></path>
+//           </svg>
+//         </Link>
+//       </div>
 
-      <h1 className="text-center text-xl font-semibold mt-5 text-white">
-        Create QRcode
-      </h1>
+//       <h1 className="text-center text-xl font-semibold mt-5 text-white">
+//         Create QRcode
+//       </h1>
 
-      <div className="flex items-center justify-between mt-8 space-x-3 bg-lightCulture p-4 rounded-md border border-culture">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-          viewBox="-5.0 -10.0 110.0 135.0"
-          width={35}
-          fill="#fff"
-        >
-          <g>
-            <path d="m47.398 78.398c-8.3008 0-16.102-3.1992-21.898-9.1016-5.8984-5.8008-9.1016-13.602-9.1016-21.898 0-8.3008 3.1992-16.102 9.1016-21.898 5.8984-5.8008 13.602-9.1016 21.898-9.1016 8.3008 0 16.102 3.1992 21.898 9.1016 5.8984 5.8984 9.1016 13.602 9.1016 21.898 0 8.3008-3.1992 16.102-9.1016 21.898-5.7969 5.9023-13.598 9.1016-21.898 9.1016zm0-56.797c-6.8984 0-13.398 2.6992-18.301 7.6016-4.8984 4.8984-7.6016 11.398-7.6016 18.301 0 6.8984 2.6992 13.398 7.6016 18.301 4.8984 4.8984 11.398 7.6016 18.301 7.6016 6.8984 0 13.398-2.6992 18.301-7.6016 4.8984-4.8984 7.6016-11.398 7.6016-18.301s-2.6992-13.504-7.5-18.402c-4.9023-4.8008-11.402-7.5-18.402-7.5z" />
-            <path d="m65.746 69.336 3.6055-3.6055 14.141 14.141-3.6055 3.6055z" />
-          </g>
-        </svg>{" "}
-        <input
-          placeholder="Search QR code type"
-          className="w-full bg-transparent outline-none border-none text-white"
-        />
-      </div>
+//       <div className="flex items-center justify-between mt-8 space-x-3 bg-lightCulture p-4 rounded-md border border-culture">
+//         <svg
+//           xmlns="http://www.w3.org/2000/svg"
+//           version="1.1"
+//           viewBox="-5.0 -10.0 110.0 135.0"
+//           width={35}
+//           fill="#fff"
+//         >
+//           <g>
+//             <path d="m47.398 78.398c-8.3008 0-16.102-3.1992-21.898-9.1016-5.8984-5.8008-9.1016-13.602-9.1016-21.898 0-8.3008 3.1992-16.102 9.1016-21.898 5.8984-5.8008 13.602-9.1016 21.898-9.1016 8.3008 0 16.102 3.1992 21.898 9.1016 5.8984 5.8984 9.1016 13.602 9.1016 21.898 0 8.3008-3.1992 16.102-9.1016 21.898-5.7969 5.9023-13.598 9.1016-21.898 9.1016zm0-56.797c-6.8984 0-13.398 2.6992-18.301 7.6016-4.8984 4.8984-7.6016 11.398-7.6016 18.301 0 6.8984 2.6992 13.398 7.6016 18.301 4.8984 4.8984 11.398 7.6016 18.301 7.6016 6.8984 0 13.398-2.6992 18.301-7.6016 4.8984-4.8984 7.6016-11.398 7.6016-18.301s-2.6992-13.504-7.5-18.402c-4.9023-4.8008-11.402-7.5-18.402-7.5z" />
+//             <path d="m65.746 69.336 3.6055-3.6055 14.141 14.141-3.6055 3.6055z" />
+//           </g>
+//         </svg>{" "}
+//         <input
+//           placeholder="Search QR code type"
+//           className="w-full bg-transparent outline-none border-none text-white"
+//         />
+//       </div>
 
-      <h2 className="text-center text-lg mt-5 text-white">
-        Select your QR code type
-      </h2>
+//       <h2 className="text-center text-lg mt-5 text-white">
+//         Select your QR code type
+//       </h2>
 
-      <div className="flex justify-center items-center gap-5 flex-wrap mt-8">
-        {selectQrcode.map((select, i) => (
-          <div
-            onClick={() => handleSelectFormat(i, select.type)}
-            key={i}
-            className={`${
-              selectFormat === i ? "bg-red" : "bg-grey"
-            } w-[150px] rounded-md flex flex-col items-center justify-center py-5 border`}
-          >
-            <div className="flex justify-center items-center w-[30px]">
-              <Image src={select.icon} alt="qrcode format icons" />
-            </div>
-            <p
-              className={`${
-                selectFormat === i ? "text-white" : "text-black"
-              } mt-4`}
-            >
-              {select.type}
-            </p>
-          </div>
-        ))}
-      </div>
-    </Wrapper>
-  );
-}
+//       <div className="flex justify-center items-center gap-5 flex-wrap mt-8">
+//         {selectQrcode.map((select, i) => (
+//           <div
+//             onClick={() => handleSelectFormat(i, select.type)}
+//             key={i}
+//             className={`${
+//               selectFormat === i ? "bg-red" : "bg-grey"
+//             } w-[150px] rounded-md flex flex-col items-center justify-center py-5 border`}
+//           >
+//             <div className="flex justify-center items-center w-[30px]">
+//               <Image src={select.icon} alt="qrcode format icons" />
+//             </div>
+//             <p
+//               className={`${
+//                 selectFormat === i ? "text-white" : "text-black"
+//               } mt-4`}
+//             >
+//               {select.type}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </Wrapper>
+//   );
+// }
--- app/pages/final_page/page.jsx
+++ app/pages/final_page/page.jsx
@@ -1,130 +1,130 @@
-"use client";
+// "use client";
 
-import Nav from "@/components/Nav";
-import Wrapper from "@/components/Wrapper";
-import { useRouter } from "next/navigation";
-import { RotatingLines } from "react-loader-spinner";
-import { exportContext } from "@/components/useStateContext/StateContext";
+// import Nav from "@/components/Nav";
+// import Wrapper from "@/components/Wrapper";
+// import { useRouter } from "next/navigation";
+// import { RotatingLines } from "react-loader-spinner";
+// import { exportContext } from "@/components/useStateContext/StateContext";
 
-export default function page() {
-  const {
-    src,
-    url,
-    type,
-    showSpinner,
-    qrcodeName,
-    saveToHistory,
-    qrcodeHistory,
-  } = exportContext();
+// export default function page() {
+//   const {
+//     src,
+//     url,
+//     type,
+//     showSpinner,
+//     qrcodeName,
+//     saveToHistory,
+//     qrcodeHistory,
+//   } = exportContext();
 
-  const router = useRouter();
-  const goBack = () => {
-    router.back();
-  };
+//   const router = useRouter();
+//   const goBack = () => {
+//     router.back();
+//   };
 
-  const handleQrcodeDownload = () => {
-    const fileUrl = src;
-    const link = document.createElement("a");
-    link.href = fileUrl;
-    link.download = `${qrcodeName} QRcode`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+//   const handleQrcodeDownload = () => {
+//     const fileUrl = src;
+//     const link = document.createElement("a");
+//     link.href = fileUrl;
+//     link.download = `${qrcodeName} QRcode`;
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//   };
 
-  const qrcodeOptions = [
-    {
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" id="download"><path fill="#000" d="M12 4a1 1 0 0 0-1 1v9.529l-4.218-4.223a1.043 1.043 0 0 0-1.476 0 1.046 1.046 0 0 0 0 1.478l5.904 5.91c.217.217.506.319.79.305.284.014.573-.088.79-.305l5.904-5.91a1.046 1.046 0 0 0 0-1.478 1.043 1.043 0 0 0-1.476 0L13 14.529V5a1 1 0 0 0-1-1zM5 21a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1z"></path></svg>`,
-      label: "Download",
-      onClick: () => handleQrcodeDownload(),
-    },
-    {
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="share"><path fill="#222" d="M6.54 55.08a1.91 1.91 0 0 1-.62-.1 2 2 0 0 1-1.38-2c0-.3 2.06-29.34 31.18-31.62V10.92a2 2 0 0 1 3.43-1.4l19.74 20.16a2 2 0 0 1 0 2.8L39.15 52.64a2 2 0 0 1-3.43-1.4V41c-19.44.74-27.41 13-27.49 13.15a2 2 0 0 1-1.69.93Zm33.18-39.26v7.41a2 2 0 0 1-1.93 2c-18.84.69-25.58 13.24-28 21.31 5-4.32 13.91-9.6 27.81-9.6h.09a2 2 0 0 1 2 2v7.41l15-15.26Z"></path></svg>`,
-      label: "Share",
-      onClick: () => null,
-    },
-    {
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" clip-rule="evenodd" viewBox="0 0 24 24" id="print"><path d="M17.5,2.978c-0,-0.828 -0.672,-1.5 -1.5,-1.5l-8,0c-0.828,0 -1.5,0.672 -1.5,1.5l0,3.5c0,0.276 0.224,0.5 0.5,0.5l10,0c0.276,0 0.5,-0.224 0.5,-0.5l0,-3.5Zm-1,0l0,3c-0,0 -9,0 -9,0c0,0 0,-3 0,-3c0,-0.276 0.224,-0.5 0.5,-0.5c0,0 8,0 8,0c0.276,0 0.5,0.224 0.5,0.5Z"></path><path d="M17,16.978l3,0c0.828,0 1.5,-0.671 1.5,-1.5l0,-8c-0,-0.828 -0.672,-1.5 -1.5,-1.5c-3.151,0 -12.849,0 -16,0c-0.828,0 -1.5,0.672 -1.5,1.5l0,8c0,0.829 0.672,1.5 1.5,1.5c0.714,0 1.765,0 3,0c0.276,0 0.5,-0.224 0.5,-0.5c0,-0.276 -0.224,-0.5 -0.5,-0.5l-3,0c-0.276,0 -0.5,-0.224 -0.5,-0.5c-0,0 0,-8 0,-8c0,-0.276 0.224,-0.5 0.5,-0.5l16,0c0.276,0 0.5,0.224 0.5,0.5c0,0 0,8 0,8c-0,0.276 -0.224,0.5 -0.5,0.5l-3,0c-0.276,0 -0.5,0.224 -0.5,0.5c0,0.276 0.224,0.5 0.5,0.5Z"></path><path d="M17.5,12.978c0,-0.276 -0.224,-0.5 -0.5,-0.5l-10,0c-0.276,0 -0.5,0.224 -0.5,0.5l0,9c0,0.276 0.224,0.5 0.5,0.5l10,0c0.276,0 0.5,-0.224 0.5,-0.5l0,-9Zm-1,0.5l0,8c-0,0 -9,0 -9,0c0,0 0,-8 0,-8l9,0Z"></path><path d="M18 12.478l-12 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l12 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM14 15.478l-4 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l4 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM14 18.478l-4 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l4 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM18 8.978l-2 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l2 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5z"></path></svg>`,
-      label: "Print",
-      onClick: () => console.log("clicked"),
-    },
-  ];
+//   const qrcodeOptions = [
+//     {
+//       icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" id="download"><path fill="#000" d="M12 4a1 1 0 0 0-1 1v9.529l-4.218-4.223a1.043 1.043 0 0 0-1.476 0 1.046 1.046 0 0 0 0 1.478l5.904 5.91c.217.217.506.319.79.305.284.014.573-.088.79-.305l5.904-5.91a1.046 1.046 0 0 0 0-1.478 1.043 1.043 0 0 0-1.476 0L13 14.529V5a1 1 0 0 0-1-1zM5 21a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1z"></path></svg>`,
+//       label: "Download",
+//       onClick: () => handleQrcodeDownload(),
+//     },
+//     {
+//       icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="share"><path fill="#222" d="M6.54 55.08a1.91 1.91 0 0 1-.62-.1 2 2 0 0 1-1.38-2c0-.3 2.06-29.34 31.18-31.62V10.92a2 2 0 0 1 3.43-1.4l19.74 20.16a2 2 0 0 1 0 2.8L39.15 52.64a2 2 0 0 1-3.43-1.4V41c-19.44.74-27.41 13-27.49 13.15a2 2 0 0 1-1.69.93Zm33.18-39.26v7.41a2 2 0 0 1-1.93 2c-18.84.69-25.58 13.24-28 21.31 5-4.32 13.91-9.6 27.81-9.6h.09a2 2 0 0 1 2 2v7.41l15-15.26Z"></path></svg>`,
+//       label: "Share",
+//       onClick: () => null,
+//     },
+//     {
+//       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" clip-rule="evenodd" viewBox="0 0 24 24" id="print"><path d="M17.5,2.978c-0,-0.828 -0.672,-1.5 -1.5,-1.5l-8,0c-0.828,0 -1.5,0.672 -1.5,1.5l0,3.5c0,0.276 0.224,0.5 0.5,0.5l10,0c0.276,0 0.5,-0.224 0.5,-0.5l0,-3.5Zm-1,0l0,3c-0,0 -9,0 -9,0c0,0 0,-3 0,-3c0,-0.276 0.224,-0.5 0.5,-0.5c0,0 8,0 8,0c0.276,0 0.5,0.224 0.5,0.5Z"></path><path d="M17,16.978l3,0c0.828,0 1.5,-0.671 1.5,-1.5l0,-8c-0,-0.828 -0.672,-1.5 -1.5,-1.5c-3.151,0 -12.849,0 -16,0c-0.828,0 -1.5,0.672 -1.5,1.5l0,8c0,0.829 0.672,1.5 1.5,1.5c0.714,0 1.765,0 3,0c0.276,0 0.5,-0.224 0.5,-0.5c0,-0.276 -0.224,-0.5 -0.5,-0.5l-3,0c-0.276,0 -0.5,-0.224 -0.5,-0.5c-0,0 0,-8 0,-8c0,-0.276 0.224,-0.5 0.5,-0.5l16,0c0.276,0 0.5,0.224 0.5,0.5c0,0 0,8 0,8c-0,0.276 -0.224,0.5 -0.5,0.5l-3,0c-0.276,0 -0.5,0.224 -0.5,0.5c0,0.276 0.224,0.5 0.5,0.5Z"></path><path d="M17.5,12.978c0,-0.276 -0.224,-0.5 -0.5,-0.5l-10,0c-0.276,0 -0.5,0.224 -0.5,0.5l0,9c0,0.276 0.224,0.5 0.5,0.5l10,0c0.276,0 0.5,-0.224 0.5,-0.5l0,-9Zm-1,0.5l0,8c-0,0 -9,0 -9,0c0,0 0,-8 0,-8l9,0Z"></path><path d="M18 12.478l-12 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l12 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM14 15.478l-4 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l4 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM14 18.478l-4 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l4 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5zM18 8.978l-2 0c-.276 0-.5.224-.5.5 0 .276.224.5.5.5l2 0c.276 0 .5-.224.5-.5 0-.276-.224-.5-.5-.5z"></path></svg>`,
+//       label: "Print",
+//       onClick: () => console.log("clicked"),
+//     },
+//   ];
 
-  return (
-    <Wrapper>
-      <Nav />
+//   return (
+//     <Wrapper>
+//       <Nav />
 
-      <div className="flex justify-between items-center mt-14 w-full">
-        <button
-          onClick={goBack}
-          className="w-[120px] h-[40px] rounded-md bg-white py-1 px-4 flex justify-center items-center space-x-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            viewBox="0 0 16 16"
-            id="chevron-left"
-          >
-            <path
-              fill="#212121"
-              d="M10.0103,3.57541 C10.3139,3.85726 10.3314,4.33181 10.0496,4.63534 L7.27348,7.625 L10.0496,10.6147 C10.3314,10.9182 10.3139,11.3927 10.0103,11.6746 C9.7068,11.9565 9.23226,11.9389 8.9504,11.6353 L5.7004,8.13534 C5.4332,7.84758 5.4332,7.40243 5.7004,7.11466 L8.9504,3.61466 C9.23226,3.31113 9.70681,3.29356 10.0103,3.57541 Z"
-            ></path>
-          </svg>{" "}
-          <p className="text-sm">Back</p>
-        </button>
+//       <div className="flex justify-between items-center mt-14 w-full">
+//         <button
+//           onClick={goBack}
+//           className="w-[120px] h-[40px] rounded-md bg-white py-1 px-4 flex justify-center items-center space-x-2"
+//         >
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             width="25"
+//             height="25"
+//             viewBox="0 0 16 16"
+//             id="chevron-left"
+//           >
+//             <path
+//               fill="#212121"
+//               d="M10.0103,3.57541 C10.3139,3.85726 10.3314,4.33181 10.0496,4.63534 L7.27348,7.625 L10.0496,10.6147 C10.3314,10.9182 10.3139,11.3927 10.0103,11.6746 C9.7068,11.9565 9.23226,11.9389 8.9504,11.6353 L5.7004,8.13534 C5.4332,7.84758 5.4332,7.40243 5.7004,7.11466 L8.9504,3.61466 C9.23226,3.31113 9.70681,3.29356 10.0103,3.57541 Z"
+//             ></path>
+//           </svg>{" "}
+//           <p className="text-sm">Back</p>
+//         </button>
 
-        <button
-          onClick={() => saveToHistory(url, type, src, qrcodeName)}
-          className="w-[120px] h-[40px] rounded-md bg-red py-1 px-4 flex justify-center items-center space-x-2"
-        >
-          <p className="text-white text-sm">Finish</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            id="chevron-right"
-            width={35}
-            height={35}
-            fill="#FFFFFF"
-          >
-            <path d="M10,17a1,1,0,0,1-.71-1.71L12.59,12,9.29,8.71a1,1,0,0,1,1.41-1.41l4,4a1,1,0,0,1,0,1.41l-4,4A1,1,0,0,1,10,17Z"></path>
-          </svg>
-        </button>
-      </div>
+//         <button
+//           onClick={() => saveToHistory(url, type, src, qrcodeName)}
+//           className="w-[120px] h-[40px] rounded-md bg-red py-1 px-4 flex justify-center items-center space-x-2"
+//         >
+//           <p className="text-white text-sm">Finish</p>
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 24 24"
+//             id="chevron-right"
+//             width={35}
+//             height={35}
+//             fill="#FFFFFF"
+//           >
+//             <path d="M10,17a1,1,0,0,1-.71-1.71L12.59,12,9.29,8.71a1,1,0,0,1,1.41-1.41l4,4a1,1,0,0,1,0,1.41l-4,4A1,1,0,0,1,10,17Z"></path>
+//           </svg>
+//         </button>
+//       </div>
 
-      <h1 className="text-center text-white text-xl font-semibold mt-5">
-        Congratulations!
-      </h1>
-      <h3 className="text-white font-semibold text-center mt-5">
-        You've created a QRcode!
-      </h3>
+//       <h1 className="text-center text-white text-xl font-semibold mt-5">
+//         Congratulations!
+//       </h1>
+//       <h3 className="text-white font-semibold text-center mt-5">
+//         You've created a QRcode!
+//       </h3>
 
-      <div className="flex justify-center items-center mt-10">
-        {showSpinner && <RotatingLines strokeColor="grey" />}
-      </div>
+//       <div className="flex justify-center items-center mt-10">
+//         {showSpinner && <RotatingLines strokeColor="grey" />}
+//       </div>
 
-      <div className="flex justify-center w-full">
-        <div
-          className={`flex flex-col justify-center items-center bg-lightCulture p-5 rounded-xl shadow-md`}
-        >
-          <img src={src} alt="qrcode image" />
-        </div>
-      </div>
+//       <div className="flex justify-center w-full">
+//         <div
+//           className={`flex flex-col justify-center items-center bg-lightCulture p-5 rounded-xl shadow-md`}
+//         >
+//           <img src={src} alt="qrcode image" />
+//         </div>
+//       </div>
 
-      <div className="flex justify-center items-center w-full gap-2 mt-10">
-        {qrcodeOptions.map((option) => (
-          <div
-            onClick={option.onClick}
-            key={option.label}
-            className="w-1/3 flex flex-col items-center py-2 px-4 bg-red rounded-md"
-          >
-            <div className="w-5 h-5">
-              <div dangerouslySetInnerHTML={{ __html: option.icon }} />
-            </div>
-            <h4 className="text-xs mt-2 text-white">{option.label}</h4>
-          </div>
-        ))}
-      </div>
-    </Wrapper>
-  );
-}
+//       <div className="flex justify-center items-center w-full gap-2 mt-10">
+//         {qrcodeOptions.map((option) => (
+//           <div
+//             onClick={option.onClick}
+//             key={option.label}
+//             className="w-1/3 flex flex-col items-center py-2 px-4 bg-red rounded-md"
+//           >
+//             <div className="w-5 h-5">
+//               <div dangerouslySetInnerHTML={{ __html: option.icon }} />
+//             </div>
+//             <h4 className="text-xs mt-2 text-white">{option.label}</h4>
+//           </div>
+//         ))}
+//       </div>
+//     </Wrapper>
+//   );
+// }
